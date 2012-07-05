ab70213c2f6ee62f34155a29526b6e928f916fe6
enorhe <enorhe@5457ac6f-33fd-4ed3-ad72-7c75575839e5>
2012-07-05 08:56:50 +0000

    added __init__.py
    git-svn-id: https://bebiservice.umb.no/svn-private/pyLPA@3 5457ac6f-33fd-4ed3-ad72-7c75575839e5

---
+++ trunk/pyLPA/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+'''Initialization of pyLPA, a module for analyzing laminar data by
+decomposition into laminar populations.
+
+See "Einevoll et al., 2007. Laminar population analysis: estimating firing rates and 
+evoked synaptic activity from multielectrode recordings in rat barrel cortex. 
+J Neurophysiol. 97(3):2174-90" for a description of the methodology.
+'''
